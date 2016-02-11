c230592e4d6fc12738d719ed2f68642cda262587
Kaitlin Seligson <kseligso@ucsd.edu>
2016-02-11 00:39:40 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,20 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/' + idNumber, addProject);
+	console.log('/project/' + idNumber);
+}
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' +
+		'<img src="' + result['image'] + '"class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p>' +
+		'<p><small>' + result['summary'] + '</small></p></a>';
+
+	$("#project" + result['id'] + " .details").html(projectHTML);
 }
 
 /*
@@ -35,4 +49,18 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	e.preventDefault();
+
+	$.get('/palette', addColors);
+}
+
+function addColors(result) {
+	console.log(result);
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
